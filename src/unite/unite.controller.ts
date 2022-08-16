8e0f435096a24f3b9a23c5d9c0c24b9d7ec4e961
Ennoukra Abdelghani <ghaniennoukra@gmail.com>
2022-08-16 14:18:04 +0100

    create a method that return the unite of the user logedIn

--- src/unite/unite.controller.ts
+++ src/unite/unite.controller.ts
@@ -11,41 +11,49 @@ import {
 } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guard/jwtAuth.guard';
 import { Unite } from 'src/entities/Unite.entity';
+import { UserService } from 'src/user/user.service';
 import { CreateUniteDto } from './dto/createUniteDto';
 import { UpdateUniteDto } from './dto/updateUniteDto';
 import { UniteService } from './unite.service';
 
 @Controller('unite')
 export class UniteController {
-  constructor(private uniteService: UniteService) {}
+  constructor(
+    private uniteService: UniteService,
+    private userService: UserService,
+  ) {}
 
+  // get all unites for the user logedId
   @UseGuards(JwtAuthGuard)
   @Get('')
-  async getAllUnite(): Promise<Unite[]> {
-    return await this.uniteService.getAllUnite();
+  async getUnitesByUserLogedIn(@Body() data) {
+    return await this.userService.getUniteByUserLogedIn(data.user);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post('create')
   async ceateUnite(@Body() createUniteDto: CreateUniteDto) {
     return await this.uniteService.createUnite(createUniteDto);
   }
-
+  @UseGuards(JwtAuthGuard)
   @Get('search')
   async findUnites(@Query() query) {
     console.log(query);
     return this.uniteService.findByQuery(query);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async getUniteById(@Param('id', ParseIntPipe) id: number) {
     return await this.uniteService.getUniteById(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post('update')
   async updateUnite(@Body() updateUniteDto: UpdateUniteDto) {
     return await this.uniteService.updateUnite(updateUniteDto);
   }
-
+  @UseGuards(JwtAuthGuard)
   @Delete('delete')
   async deleteUnite(@Param('id', ParseIntPipe) id: number) {
     return await this.uniteService.deleteUnite(id);
--- src/unite/unite.module.ts
+++ src/unite/unite.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Unite } from 'src/entities/Unite.entity';
 import { UniteService } from './unite.service';
 import { UniteController } from './unite.controller';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Unite])],
+  imports: [TypeOrmModule.forFeature([Unite]), UserModule],
   providers: [UniteService],
   exports: [UniteService],
   controllers: [UniteController],
