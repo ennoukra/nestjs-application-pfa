ba87d8c7e9c0752060173b4e699b5d5f6706f5de
Ennoukra Abdelghani <ghaniennoukra@gmail.com>
2022-09-02 22:26:12 +0100

    add parcelle service

--- src/parcelle/parcelle.module.ts
+++ src/parcelle/parcelle.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ParcelleService } from './parcelle.service';
 
-@Module({})
+@Module({
+  providers: [ParcelleService]
+})
 export class ParcelleModule {}
