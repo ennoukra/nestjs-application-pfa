51cea8103df702bee00dd43d7a252fa1e872d713
Ennoukra Abdelghani <ghaniennoukra@gmail.com>
2022-09-03 22:54:11 +0100

    change in parcelle model

--- src/parcelle/parcelle.module.ts
+++ src/parcelle/parcelle.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ParcelleService } from './parcelle.service';
+import { ParcelleController } from './parcelle/parcelle.controller';
+import { ParcelleController } from './parcelle.controller';
 
 @Module({
-  providers: [ParcelleService]
+  providers: [ParcelleService],
+  controllers: [ParcelleController]
 })
 export class ParcelleModule {}
